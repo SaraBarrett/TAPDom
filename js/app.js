777a271ce484fba1d56c6f27f6463b04e9ae7fde
SaraBarrett <sara.c.mon@gmail.com>
2024-09-27 13:03:31 +0100

    manipular elementos

---
+++ js/app.js
@@ -0,0 +1,21 @@
+// let myBannerSelector = document.getElementById('banner');
+
+// console.log(myBannerSelector);
+
+// let mySelectedImage = document.getElementById('unicorn');
+
+// let mySelectedHeading = document.getElementById('mainheading');
+
+// console.log(mySelectedImage);
+// console.log(mySelectedHeading);
+
+// let allImages = document.querySelectorAll('img');
+// console.log(allImages);
+
+// let doneTodos = document.querySelectorAll('.done');
+// let myCheckbox = document.querySelector("input[type='checkbox']")
+
+// console.log(myCheckbox);
+
+let myText = document.getElementById('History');
+myText.innerText = 'História';
