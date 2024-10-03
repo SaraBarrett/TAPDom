06a64b839b6cf015dce60f5c26e9733b3a14a4e8
SaraBarrett <sara.c.mon@gmail.com>
2024-10-03 16:31:57 +0100

    manipular

--- js/app.js
+++ js/app.js
@@ -17,5 +17,54 @@
 
 // console.log(myCheckbox);
 
-let myText = document.getElementById('History');
-myText.innerText = 'História';
+// let myText = document.getElementById('History');
+// let mySpan = document.getElementById('my-welcome-name');
+//let myPickles =document.querySelector('span');
+
+
+// console.log(myPhoto)
+// if(myBannerSelector){
+//     //console.log(myBannerSelector.getAttribute('src'));
+//     console.log(myBannerSelector.src);
+//     myBannerSelector.src = '../images/assesement.jpeg';
+// }
+
+
+// if(myPickles){
+//     myPickles.innerText = 'yack';
+// }
+
+// if(myText){
+//     myText.innerHTML = '<b>História da Carochinha</b>';
+//     console.log(myText.getAttribute('id'))
+//     myText.id = 'testesMudarAtributos';
+// }
+
+
+
+let login = false;
+let myName = null;
+let userPhoto = null;
+
+let mySpan = document.getElementById('my-welcome-name');
+let myPhoto = document.getElementById('my-photo');
+
+myName = prompt('qual é o teu nome?');
+
+if(myName){
+    login == true;
+    userPhoto = '../images/assesement.jpeg';
+
+
+    mySpan.innerText = myName;
+
+    if(userPhoto){
+        myPhoto.src = userPhoto;
+    }
+
+}
+
+
+
+
+
