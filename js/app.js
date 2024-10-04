e80637ea50eca08934206881361062e3047c4a66
SaraBarrett <sara.c.mon@gmail.com>
2024-10-04 10:12:26 +0100

    magical forest

--- js/app.js
+++ js/app.js
@@ -42,29 +42,55 @@
 
 
 
-let login = false;
-let myName = null;
-let userPhoto = null;
+// let login = false;
+// let myName = null;
+// let userPhoto = null;
 
-let mySpan = document.getElementById('my-welcome-name');
-let myPhoto = document.getElementById('my-photo');
+// let mySpan = document.getElementById('my-welcome-name');
+// let myPhoto = document.getElementById('my-photo');
 
-myName = prompt('qual é o teu nome?');
+// myName = prompt('qual é o teu nome?');
 
-if(myName){
-    login == true;
-    userPhoto = '../images/assesement.jpeg';
+// if(myName){
+//     login == true;
+//     userPhoto = '../images/assesement.jpeg';
 
 
-    mySpan.innerText = myName;
+//     mySpan.innerText = myName;
 
-    if(userPhoto){
-        myPhoto.src = userPhoto;
-    }
+//     if(userPhoto){
+//         myPhoto.src = userPhoto;
+//     }
 
+// }
+
+
+//selecciona o elemento
+// let myEggOrChickenImg = document.querySelector('img');
+
+
+// function myEggOrChicken(){
+//     if(myEggOrChickenImg.src == 'https://devsprouthosting.com/images/chicken.jpg'){
+//         myEggOrChickenImg.src = 'https://devsprouthosting.com/images/egg.jpg';
+//     }else{
+//         //muda o atributo src do elemento seleccionado
+//         myEggOrChickenImg.src = 'https://devsprouthosting.com/images/chicken.jpg';
+
+//     }
+
+// }
+
+
+//seleccionar o olá do index.html
+// let myBold = document.querySelector('b');
+// myBold.style.color = '#132fba';
+
+
+function changeStyle(){
+    let myContainer = document.querySelector('#container');
+    let myImg = document.querySelector('img');
+
+    myContainer.style.textAlign = 'center';
+    myImg.style.width = '150px';
+    myImg.style.borderRadius = '50%';
 }
-
-
-
-
-
